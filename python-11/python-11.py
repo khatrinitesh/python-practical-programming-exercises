5a87f53c2642e830721db9a27c9a5739c21408d8
nitesh <nitesh.khatri88@gmail.com>
2026-06-02 18:10:53 +0530

    updatd

---
+++ python-11/python-11.py
@@ -0,0 +1,32 @@
+# print details write a program to print your name, age and city 
+# name = 'test'
+# age = 37 
+# city = 'mumbai'
+# print(f'Name: {name}, Age: {age}, City: {city}')
+
+# add two numbers - take two numbers and print their sum also calculate subtraction, multiplication and division of two numbers 
+# a = 10 
+# b = 20 
+# sum_value = a + b 
+# print(f'sum of {a} and {b} is {sum_value}')
+
+# check even or odd - change the number and test different values.
+number = 7 
+if number % 2 == 0: 
+    print(f'{number} is even')
+else:
+    print(f'{number} is odd')   
+
+# find the largest number 
+# a = 15 
+# b = 30
+# c = 20 
+# if a>=b and a>=c:
+#     print(f'{a} is the largest number')
+# elif b>=a and b>=c:
+#     print(f'{b} is the largest number')
+
+# print numbers from 1 to 10 using a loop
+# for i in range(1,11):
+#     print(f'number is {i}')
+
