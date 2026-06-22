dab557ad74b57eb999e461b9e11d3584c247b3a1
nitesh <nitesh.khatri88@gmail.com>
2026-06-22 18:09:17 +0530

    updated

---
+++ python-14.py
@@ -0,0 +1,92 @@
+# print("Hello Python")
+
+# print('my name is _____')
+# print('my age is _____')
+# print('my city is _____')
+
+# name = 'nitesh'
+# age = 37
+# city = 'mumbai'
+# print(name)
+# print(age)
+# print(city)
+# print(f'my name {name} and my age is {age} and my city is {city}')
+
+# name = 'test'
+# age = 37
+# price = 20 
+# is_student = True 
+# print(name,age,price,is_student)
+# student_name = '____'
+# student_age = '____'
+# marks = '____'
+# is_passed = "___"
+# print(type(student_name))
+# print(type(student_age))
+# print(type(marks))
+# print(type(is_passed))
+
+# name = input('enter your name')
+# print(name)
+
+# name = input('enter your name: ')
+# age = input('enter your age: ')
+# print(f'my name is {name} and age is {age}')
+
+# age = int(input("Enter your age: "))
+# print(age + 5)
+
+# num1 = int(input("Enter first number: "))
+# num2 = int(input("Enter second number: "))
+# sum = num1 + num2 
+# print(sum)
+
+# a = 10 
+# b = 5 
+
+# print(a + b)
+# print(a - b)
+# print(a * b)
+# print(a / b)
+
+# a = int(input("Enter first number: "))
+# b = int(input("Enter second number: "))
+# print("Addition:", a + b)
+# print("Subtraction:", a - b)
+# print("Multiplication:", a * b)
+# print("Division:", a / b)
+
+# age = 18
+# if age >=19:
+#     print('you can vote')
+# else:
+#     print('you can not vote')
+
+# num = int(input("Enter a number: "))
+# if num >= 0:
+#     print('positive number')
+# else:
+#     print('negative number')
+
+# marks = 75
+
+# if marks >= 90:
+#     print("Grade A")
+# elif marks >= 60:
+#     print("Grade B")
+# else:
+#     print("Grade C")
+
+# marks = int(input("Enter your marks: "))
+
+# if marks >= 90:
+#     print("Grade A")
+# elif marks >= 75:
+#     print("Grade B")
+# elif marks >= 50:
+#     print("Grade C")
+# else:
+#     print("Fail")
+
+for i in range(25):
+    print('test',i)
