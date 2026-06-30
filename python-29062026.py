1ba91bce9ff4edbfff4e8cbe03f45cabf70e70a8
nitesh <nitesh.khatri88@gmail.com>
2026-06-30 13:22:32 +0530

    updated

---
+++ python-29062026.py
@@ -0,0 +1,183 @@
+# print("hello world")
+# print("YAHUBABA")
+
+# print('python course')
+# print('hello world')
+# print('sum is: ',40+2)
+
+# name = 'test'
+# age = 25
+# height = 5.10
+
+# name = 'joyful journeys'
+
+# print(name)
+# print(age)
+# print(height)
+
+# user_name = 'nitesh'
+# student1 = 'alex'
+# _marks = 90 
+# myName = 'john'
+# MyName = 'john'
+
+# # 2name = 'john'
+
+# first_name = 'john'
+
+# a, b, c = 10, 20, 30 
+# a, b, c = 'orange','banana','mango'
+
+# print(a)
+# print(b)
+# print(c)
+
+# x = y =z  = 100 
+# print(x)
+# print(y)
+# print(z)
+
+# name = 'john'
+# color = 'red'
+# age = 25 
+# print('your name is ',name, ". your age is ",age,'your color is ',color)
+
+########################### DATATYPES ###########################
+# print('hello world')
+# print(-20)
+# print(4+6.23)
+# print(4+6j)
+# print(False)
+# print(10>50)
+# print(['apple','banana','mango'])
+# print(('apple','banana','mango'))
+# print(range(12));
+# print([23,44,55])
+# print({'name':'test'},{'age':5},{"course":'python'})
+# print({11,12,13}) 
+# print(frozenset([1,2,3]))
+# print(None)
+# print(b'Hello')
+# print(bytearray([97,55,85,]))
+# print(memoryview(bytes([65,58,56])))
+# x = 'nitesh'
+# x = 11
+# x = -111
+# x = [11,22,33]
+# x = (11,32333,33)
+# x = {"name":'nitesh'}
+
+# print(type(x))
+
+########################### LIST ###########################
+# fruits = ["Apple", "Banana", "Mango","Apple", "Grapes"]
+# fruits[1] = 'orange'
+# print(fruits)
+# number = [10, 20, 25, 30, 10]
+# print(number)
+# print(fruits[0])
+# print(fruits[1])
+# print(fruits[-1])
+# print(fruits[-2])
+# print(fruits[2:3])
+# data = ["Sanchit", 25, 75.50, True]
+# print(data)
+# name,age,marks,married = data 
+# print(data)
+# name = 'tst'
+# print("name: ",name)
+
+########################### TUPLES ###########################
+# fruits = ("Apple", "Banana", "Mango", "Banana")
+# veg = ("Potato", "Carrot")
+# print(fruits)
+# print(veg)
+# mix = fruits + veg 
+# print(mix)
+# print(fruits[1])
+# data = ('test',45,True)
+# print(data[0])
+
+########################### list methods ###########################
+# fruits = ["Cherry","mango","Banana","apple" ]
+# veg = ("Potato", "Tomato")
+# print(fruits)
+# print(veg)
+# print(fruits.append('pineapple'))
+# print(fruits.insert(0,45))
+# fruits.remove('Banana')
+# print(fruits)
+# fruits.pop(0)
+# print(fruits)
+# fruits.clear()
+# print(fruits)
+# fruits.sort(reverse=True)
+# print(fruits)
+# fruits.sort()
+# print(fruits)
+# print(fruits.copy())
+# print(list(fruits))
+# print(fruits[0:3])
+
+# fruits = ["cherry","mango","banana","apple","mango" ]
+# numbers = [10, 20, 6, 44, 32]
+# print(fruits.index('mango',2,6))
+
+# x = fruits.count("grapes")
+# print(x)
+# print(len(numbers))
+# print(max(numbers))
+# print(min(numbers))
+# print(max(fruits))
+# print(min(fruits))
+# print(max(fruits))
+# colors = ('red','green')
+# print(colors)
+# numbers = {10,20,30}
+# result = list(numbers)
+# print(result)
+
+# bosch = {'filtration','dashboard','diesel'}
+# result = list(bosch)
+# print(result)
+
+# numbers = {10, 20, 30}
+# result = list(numbers)
+# print(result)
+
+fruits = ("apple", "banana", "apple", "orange", "apple")
+# print(fruits)
+# fruits = "apple", "banana", "apple", "orange", "apple"
+# print(fruits)
+# nested_tuple = ((10,20), (30, 40), (50, 60))
+# print(nested_tuple)
+
+# x = fruits.index('apple',1,3)
+# print(x)
+# print(nested_tuple[1][0])
+
+# print(fruits.count('banana'))
+
+# x = numbers.index(51)
+# print(x)
+
+# numbers = [20, 34, 51, 45, 10]
+# print(numbers[1])
+# print(max(numbers))
+# print(min(numbers))
+# print(len(numbers))
+
+# r = range(5)
+# print(r)
+# t = tuple(r)
+# print(t)
+
+# student = { 
+#    "name" : "John", 
+#    "age" : 20,
+#    "marks" : 89.45,
+#    "passed": True,
+#    "skills" : ["Python", "Javascript", "React"]
+# }
+# print(student['name'])
+
